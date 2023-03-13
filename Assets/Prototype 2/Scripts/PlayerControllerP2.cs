79ccdc14a853e1387a72d07bc94f546a9fe5b11d
Lilli <lillichuawiwat@gmail.com>
2023-03-13 18:30:47 +1100

    Added Movement

--- Assets/Prototype 2/Scripts/PlayerControllerP2.cs
+++ Assets/Prototype 2/Scripts/PlayerControllerP2.cs
@@ -3,54 +3,115 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerP2 : MonoBehaviour
+public class PlayerControllerP2 : GameBehaviour
 {
+    [Header("Movement Setting")]
+    public CharacterController characterController;
+    public Transform cam;
+    public float speed = 6f;
 
-    public enum Direction { Up, Down, Left, Right }
+    //directinal
+    public float turnSmoothTime = 0.1f;
 
-    public float moveDistance = 3;
-    public float tweenTime = 1;
+    //jumping
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded;
+
+    public GameObject firingPoint;
+    public GameObject bulletPrefab;
+
+    [Header("Player Stats")]
+
+    public float playerHealth = 100; //temp
+    
+    //[Header("UI")]
+
+    public GameObject testObject;
+
+    [Header("Projectile")]
+    public float bulletSpeed;
+    bool bulletShot = false;
+    public float timeBetweenShots;
 
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.D)) MovePlayer(Direction.Right);
-        if (Input.GetKeyDown(KeyCode.A)) MovePlayer(Direction.Left);
-        if (Input.GetKeyDown(KeyCode.W)) MovePlayer(Direction.Up);
-        if (Input.GetKeyDown(KeyCode.S)) MovePlayer(Direction.Down);
+        if (Input.GetMouseButton(0))
+        {
+            print("mouse 0");
+            FireProjectile();
+        }
+
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+
+        //inputs
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        characterController.Move(direction.normalized * speed * Time.deltaTime);
+
+        if (direction.magnitude >= 0.1f)
+        {
+            
+        }
 
 
     }
 
-    void MovePlayer(Direction _direction)
+    void FireProjectile()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-        switch (_direction)
+        if (Physics.Raycast(ray, out hit))
         {
-            case Direction.Up:
-                this.transform.DOMoveZ(this.transform.position.z + moveDistance, tweenTime).SetEase(moveEase));
-                break;
-            case Direction.Down:
-                this.transform.DOMoveZ(this.transform.position.z - moveDistance, tweenTime).SetEase(moveEase));
-                break;
-            case Direction.Left:
-                this.transform.DOMoveX(this.transform.position.x - moveDistance, tweenTime).SetEase(moveEase));
-                break;
-            case Direction.Right:
-                this.transform.DOMoveX(this.transform.position.x + moveDistance, tweenTime).SetEase(moveEase));
-                break;
+
+            if(!bulletShot)
+            {
+                Vector3 target = hit.point;
+                target = new Vector3(target.x, 0.5f, target.z);
+                testObject.transform.position = target;
+
+
+                //spawn bullet
+                Transform bulletTransform = Instantiate(bulletPrefab, firingPoint.transform.position, Quaternion.identity);
+
+                bulletShot = true;
+
+                //https://www.youtube.com/watch?v=Nke5JKPiQTw 6:22
+            }
         }
 
+        if (bulletShot) ExecuteAfterSeconds(timeBetweenShots, () => ResetBullet());
 
+    }
 
+    void ResetBullet()
+    {
+        bulletShot = false;
     }
 
 }
